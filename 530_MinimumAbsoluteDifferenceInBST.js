da4f20fb9b6e6b066b8bbfa3781086d1c2cd1287
Ji Kim <jiyoung.kimmy@gmail.com>
2023-06-15 23:49:41 -0400

    leetcode of the day 6.15.2023

---
+++ 530_MinimumAbsoluteDifferenceInBST.js
@@ -0,0 +1,10 @@
+// Prompt:
+/*
+Given the root of a Binary Search Tree (BST), return the minimum absolute difference between the values of any two different nodes in the tree.
+*/
+
+// notes
+/*
+Thoughts on how to proceed:
+1.
+*/
